0d96df25d12a568e3cadba602a21aca5e9cbc3de
Carl Theuns <carl.theuns@gmail.com>
2024-04-13 16:36:17 +0200

    story 8 ready

--- src/main/java/com/example/eurder/controllers/CustomerController.java
+++ src/main/java/com/example/eurder/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import com.example.eurder.services.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -39,5 +41,10 @@ private CustomerMapper customerMapper;
         return customerService.getAllCustomers().stream()
                 .map(customerMapper::toDto)
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public CustomerDto getCustomer(@PathVariable String id){
+        return customerMapper.toDto(customerService.getCustomer(UUID.fromString(id)));
     }
 }
